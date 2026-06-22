811b9275f16988f42b482ac0009072331da71852
Юра Гуцалюк <hutsaluck@gmail.com>
2026-06-22 22:24:44 +0300

    Corrected redesign project

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,5 +1,5 @@
-import {FC} from 'react';
-import {Outlet} from "react-router-dom";
+import {FC, useEffect, useLayoutEffect} from 'react';
+import {Outlet, useLocation} from "react-router-dom";
 
 import {Footer, Header} from "../../components";
 import css from './MainLayout.module.css';
@@ -7,6 +7,26 @@ import {useAppSelector} from "../../hooks";
 
 const MainLayout: FC = () => {
     const {lightTheme} = useAppSelector(state => state.movies);
+    const {pathname} = useLocation();
+
+    useEffect(() => {
+        if (!('scrollRestoration' in window.history)) {
+            return;
+        }
+
+        const previousScrollRestoration = window.history.scrollRestoration;
+        window.history.scrollRestoration = 'manual';
+
+        return () => {
+            window.history.scrollRestoration = previousScrollRestoration;
+        };
+    }, []);
+
+    useLayoutEffect(() => {
+        window.scrollTo(0, 0);
+        document.documentElement.scrollTop = 0;
+        document.body.scrollTop = 0;
+    }, [pathname]);
 
     return (
         <div className={`${css.MainLayout} ${lightTheme ? css.MainLayoutLight : css.MainLayoutDark}`}>
